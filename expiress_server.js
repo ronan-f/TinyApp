d15db2a92911c19e03901888f353e9a5c6ace95d
Ronan Fegan <rfegan13@gmail.com>
2019-03-12 19:46:58 +0000

    Added URLs loop plus basic HTML features

--- expiress_server.js
+++ expiress_server.js
@@ -4,13 +4,11 @@ var PORT = 8080;
 
 app.set("view engine", "ejs");
 
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+
 };
 
 app.get("/", (req, res) => {
@@ -25,6 +23,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
