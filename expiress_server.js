cc4d7d7d1b349c287a4e7ca5661ae83c5de4c914
Ronan Fegan <rfegan13@gmail.com>
2019-03-13 18:41:38 +0000

    Added delete form that removes URL from database

--- expiress_server.js
+++ expiress_server.js
@@ -22,6 +22,12 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  let short = randomStr();
+  urlDatabase[short] = req.body.longURL;
+  res.redirect('/urls/' + short);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -50,11 +56,15 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  let short = randomStr();
-  urlDatabase[short] = req.body.longURL;
-  res.redirect('/urls/' + short);
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  let shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
