54cc1a53963ecfad6fefb01c108fbf50c6587b63
Ronan Fegan <rfegan13@gmail.com>
2019-03-15 02:32:23 +0000

    Finished all of the basic requriements

--- expiress_server.js
+++ expiress_server.js
@@ -2,19 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
-
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['5d41402abc4b2a76b9719d911017c592'],
+}));
 
 function randomStr() {
   let random = '';
   const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (let i = 0; i < 6; i++)
     random += possible.charAt(Math.floor(Math.random() * possible.length));
-
   return random;
 }
 
@@ -37,7 +40,7 @@ const users = {
 };
 
 app.use((req, res, next) => {
-  app.locals.username = users[req.cookies.id];
+  app.locals.username = users[req.session.id];
   next();
 })
 
@@ -61,16 +64,14 @@ function emailCheck(input){
 function passwordTest(email, pass){
   for(user in users){
     if(users[user]['email'] === email){
-      return(users[user]['password'] === pass)
+      return(users[user]['password'], pass );
     }
   } return false;
 }
 
-
-
 app.post("/urls", (req, res) => {
   const short = randomStr();
-  urlDatabase[short] = {longURL: req.body.longURL, userID: req.cookies.id};
+  urlDatabase[short] = {longURL: req.body.longURL, userID: req.session.id};
   res.redirect('/urls/' + short);
 });
 
@@ -87,11 +88,11 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlsForUser(req.cookies.id),
-                        ID: req.cookies.id
+  let templateVars = { urls: urlsForUser(req.session.id),
+                        ID: req.session.id
                       };
 
-  if(req.cookies.id){
+  if(req.session.id){
     res.render("urls_index", templateVars);
   } else {
     res.redirect('/login');
@@ -99,7 +100,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  if(req.cookies.id){
+  if(req.session.id){
     res.render("urls_new");
   } else {
     res.redirect("/login");
@@ -107,14 +108,13 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(urlDatabase);
   let shortURL = req.params.shortURL;
   let longURL = urlDatabase[shortURL].longURL;
   let templateVars = { shortURL: shortURL, longURL: longURL};
 
-  if(req.cookies.id === urlDatabase[shortURL].userID){
+  if(req.session.id === urlDatabase[shortURL].userID){
     res.render("urls_show", templateVars);
-  } else if(req.cookies.id){
+  } else if(req.session.id){
     res.send('This URL does not belong to you!!!')
   } else {
     res.redirect("/login");
@@ -128,10 +128,10 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   let shortURL = req.params.shortURL;
-  if(req.cookies.id === urlDatabase[shortURL].userID){
+  if(req.session.id === urlDatabase[shortURL].userID){
     delete urlDatabase[shortURL];
     res.redirect("/urls");
-  } else if(req.cookies.id){
+  } else if(req.session.id){
     res.redirect("/urls");
   } else {
     res.redirect("/login");
@@ -147,7 +147,9 @@ app.post("/urls/:id", (req, res) => {
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const id = req.cookies.id;
+  const id = req.session.id;
+
+  console.log()
 
 
 if(!emailCheck(email)){
@@ -157,14 +159,14 @@ if(!emailCheck(email)){
   res.status(403);
   res.send('Error 403 - Incorrect Password');
 } else {
-  res.cookie('id', users[user].id);
+  req.session.id = users[user].id;
   res.redirect("/urls");
   }
 });
 
 app.post("/logout", (req, res) => {
-  const id = req.cookies.id;
-  res.clearCookie('id', users[id].id);
+  const id = req.session.id;
+  req.session.id = null;
   res.redirect("/urls");
 });
 
@@ -181,6 +183,7 @@ app.post("/register", (req, res) => {
   const randomID = randomStr();
   const email = req.body.email;
   const password = req.body.password;
+  const hashedPass = bcrypt.hashSync(password, 10);
   const emailFunc = emailCheck(email);
 
   if(emailCheck(email)){
@@ -195,22 +198,13 @@ app.post("/register", (req, res) => {
   users[randomID] = {
     id: randomID,
     email: email,
-    password: password
+    password: hashedPass
   };
-  res.cookie('id', randomID);
+  req.session.id = randomID;
   res.redirect("/urls");
   }
 });
 
-app.post("/register", (req, res) => {
-})
-
-
 app.listen(PORT, () => {
   console.log('connected to port', PORT);
 });
-
-
-
-
-
